fc6bbd91060a79118a98d45d426c0adb1228c68e
Runemoro <runemoro@live.ca>
2018-07-26 20:03:55 -0400

    Allow IllegalArgumentException too when looking for datafixer

--- src/main/java/org/dimdev/rift/mixin/hook/MixinEntityType.java
+++ src/main/java/org/dimdev/rift/mixin/hook/MixinEntityType.java
@@ -1,21 +1,56 @@
 package org.dimdev.rift.mixin.hook;
 
+import com.mojang.datafixers.DataFixUtils;
+import com.mojang.datafixers.types.Type;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.world.biome.Biome;
-import org.dimdev.rift.listener.BiomeAdder;
+import net.minecraft.util.SharedConstants;
+import net.minecraft.util.datafix.DataFixesManager;
+import net.minecraft.util.datafix.TypeReferences;
+import net.minecraft.world.World;
+import org.apache.logging.log4j.Logger;
 import org.dimdev.rift.listener.EntityTypeAdder;
 import org.dimdev.riftloader.RiftLoader;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.function.Function;
+
 @Mixin(EntityType.class)
 public abstract class MixinEntityType {
+    @Shadow @Final private static Logger LOGGER;
+
     @Inject(method = "<clinit>", at = @At("RETURN"))
     private static void onRegisterEntityTypes(CallbackInfo ci) {
         for (EntityTypeAdder entityTypeAdder : RiftLoader.instance.getListeners(EntityTypeAdder.class)) {
             entityTypeAdder.registerEntityTypes();
+        }
+    }
+
+    @Mixin(EntityType.Builder.class)
+    public abstract static class Builder<T extends Entity> {
+        @Shadow private boolean field_200710_b;
+
+        @Shadow @Final private Class<? extends T> entityClass;
+        @Shadow @Final private Function<? super World, ? extends T> factory;
+        @Shadow private boolean field_200711_c;
+
+        public EntityType<T> build(String id) {
+            Type<?> dataFixerType = null;
+
+            if (field_200710_b) {
+                try {
+                    dataFixerType = DataFixesManager.getDataFixer().getSchema(DataFixUtils.makeKey(1519)).getChoiceType(TypeReferences.ENTITY_TREE, id);
+                } catch (IllegalStateException | IllegalArgumentException ignored) {
+                    LOGGER.debug("No data fixer registered for entity {}", id);
+                }
+            }
+
+            return new EntityType<T>(entityClass, factory, field_200710_b, field_200711_c, dataFixerType);
         }
     }
 }
--- src/main/java/org/dimdev/rift/mixin/hook/MixinTileEntityType.java
+++ src/main/java/org/dimdev/rift/mixin/hook/MixinTileEntityType.java
@@ -1,15 +1,44 @@
 package org.dimdev.rift.mixin.hook;
 
+import com.mojang.datafixers.DataFixUtils;
+import com.mojang.datafixers.types.Type;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.datafix.DataFixesManager;
+import net.minecraft.util.datafix.TypeReferences;
+import net.minecraft.util.registry.RegistryNamespaced;
+import org.apache.logging.log4j.Logger;
 import org.dimdev.rift.listener.TileEntityTypeAdder;
 import org.dimdev.riftloader.RiftLoader;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TileEntityType.class)
 public abstract class MixinTileEntityType {
+    @Shadow @Final private static Logger LOGGER;
+    @Shadow @Final public static RegistryNamespaced<ResourceLocation, TileEntityType<?>> REGISTRY;
+
+    @Overwrite
+    public static <T extends TileEntity> TileEntityType<T> registerTileEntityType(String id, TileEntityType.Builder<T> builder) {
+        Type dataFixerType = null;
+
+        try {
+            dataFixerType = DataFixesManager.getDataFixer().getSchema(DataFixUtils.makeKey(1519)).getChoiceType(TypeReferences.BLOCK_ENTITY, id);
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            LOGGER.debug("No data fixer registered for block entity {}", id);
+        }
+
+        TileEntityType<T> tileEntityType = builder.build(dataFixerType);
+        REGISTRY.putObject(new ResourceLocation(id), tileEntityType);
+        return tileEntityType;
+    }
+
     @Inject(method = "<clinit>", at = @At("RETURN"))
     private static void onRegisterTileEntityTypes(CallbackInfo ci) {
         for (TileEntityTypeAdder entityTypeAdder : RiftLoader.instance.getListeners(TileEntityTypeAdder.class)) {
