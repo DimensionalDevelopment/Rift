d3658ae6b4bce5e2ceb1db5007c7f05902171dda
UpcraftLP <j-schirra@gmx.de>
2018-07-30 07:48:48 +0200

    added chunk generator replacement hook

---
+++ src/main/java/org/dimdev/rift/listener/ChunkGeneratorReplacer.java
@@ -0,0 +1,29 @@
+package org.dimdev.rift.listener;
+
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.gen.ChunkGeneratorType;
+import net.minecraft.world.gen.IChunkGenSettings;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.IChunkGeneratorFactory;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+/**
+ * used to substitute custom {@link IChunkGenerator}s for vanilla ones<br/>
+ * <b>NOTE:</b><br/>
+ *  custom {@link WorldType}s are registered by just instantiating them<br/>
+ *  custom {@link ChunkGeneratorType}s are registered via {@link ChunkGeneratorType#registerChunkGeneratorType(String, IChunkGeneratorFactory, ChunkGeneratorType.Settings, boolean)}<br/>
+ *  custom {@link ChunkGeneratorType.Settings} are registered via {@link ChunkGeneratorType.Settings#registerChunkGeneratorSettings(String, Supplier)}
+ */
+public interface ChunkGeneratorReplacer {
+
+    /**
+     * create an {@link IChunkGenerator} for your world type
+     * @param worldType the world type that is currently set
+     * @return a custom chunk generator or {@code null} to fall back to vanilla chunk generator selection
+     */
+    @Nullable
+    <T extends IChunkGenSettings> IChunkGenerator<T> createChunkGenerator(WorldServer world, WorldType worldType, int dimensionID);
+}
---
+++ src/main/java/org/dimdev/rift/mixin/hook/MixinWorldServer.java
@@ -0,0 +1,29 @@
+package org.dimdev.rift.mixin.hook;
+
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.gen.IChunkGenSettings;
+import net.minecraft.world.gen.IChunkGenerator;
+import org.dimdev.rift.listener.ChunkGeneratorReplacer;
+import org.dimdev.riftloader.RiftLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(WorldServer.class)
+public class MixinWorldServer {
+
+    @SuppressWarnings("ConstantConditions")
+    @Redirect(method = "createChunkProvider", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/Dimension;createChunkGenerator()Lnet/minecraft/world/gen/IChunkGenerator;"))
+    protected <T extends IChunkGenSettings> IChunkGenerator onCreateChunkGenerator(Dimension provider) {
+        WorldServer world = (WorldServer) (Object) this;
+        WorldType type = world.getWorldInfo().getTerrainType();
+        IChunkGenerator<T> generator = null;
+        for(ChunkGeneratorReplacer adder : RiftLoader.instance.getListeners(ChunkGeneratorReplacer.class)) {
+            IChunkGenerator<T> value = adder.createChunkGenerator(world, type, provider.getDimensionType().getId());
+            if(value != null) generator = value;
+        }
+        return generator != null ? generator : provider.createChunkGenerator();
+    }
+}
