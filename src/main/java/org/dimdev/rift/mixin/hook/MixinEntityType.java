290685f69f80a05e07dd8b60052a28397d891318
Runemoro <runemoro@live.ca>
2018-08-08 20:26:51 -0400

    Fix MixinEntityType

--- src/main/java/org/dimdev/rift/mixin/hook/MixinEntityType.java
+++ src/main/java/org/dimdev/rift/mixin/hook/MixinEntityType.java
@@ -4,15 +4,16 @@ import com.mojang.datafixers.DataFixUtils;
 import com.mojang.datafixers.types.Type;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.util.SharedConstants;
 import net.minecraft.util.datafix.DataFixesManager;
 import net.minecraft.util.datafix.TypeReferences;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dimdev.rift.listener.EntityTypeAdder;
 import org.dimdev.riftloader.RiftLoader;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,8 +23,6 @@ import java.util.function.Function;
 
 @Mixin(EntityType.class)
 public abstract class MixinEntityType {
-    @Shadow @Final private static Logger LOGGER;
-
     @Inject(method = "<clinit>", at = @At("RETURN"))
     private static void onRegisterEntityTypes(CallbackInfo ci) {
         for (EntityTypeAdder entityTypeAdder : RiftLoader.instance.getListeners(EntityTypeAdder.class)) {
@@ -38,7 +37,9 @@ public abstract class MixinEntityType {
         @Shadow @Final private Class<? extends T> entityClass;
         @Shadow @Final private Function<? super World, ? extends T> factory;
         @Shadow private boolean field_200711_c;
+        private static final Logger LOGGER = LogManager.getLogger();;
 
+        @Overwrite
         public EntityType<T> build(String id) {
             Type<?> dataFixerType = null;
 
