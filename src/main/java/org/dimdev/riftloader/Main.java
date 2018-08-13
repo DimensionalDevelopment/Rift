4b64e4efde69890850298154f15c12ae30a1e5f6
Runemoro <runemoro@live.ca>
2018-08-13 09:54:57 -0400

    Improve installer
     - Create mods folder
     - Automatically add or update "rift" profile and switch to it (closes #6 )
     - Use %appdata% environment variable (closes #16 )

--- src/main/java/org/dimdev/riftloader/Main.java
+++ src/main/java/org/dimdev/riftloader/Main.java
@@ -14,7 +14,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 public class Main {
-    private static final String[] libraries = {
+    private static final String[] LIBRARIES = {
             "https://www.dimdev.org/maven/org/dimdev/mixin/0.7.11-SNAPSHOT/mixin-0.7.11-SNAPSHOT.jar",
             "https://repo1.maven.org/maven2/org/ow2/asm/asm/6.2/asm-6.2.jar",
             "https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/6.2/asm-commons-6.2.jar",
@@ -22,7 +22,7 @@ public class Main {
             "https://libraries.minecraft.net/net/minecraft/launchwrapper/1.12/launchwrapper-1.12.jar"
     };
     public static final String VANILLA_SERVER = "https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar";
-    public static final String SPIGOT_SERVER = "https://cdn.getbukkit.org/spigot/spigot-1.13.jar";
+    // public static final String SPIGOT_SERVER = "https://cdn.getbukkit.org/spigot/spigot-1.13.jar";
 
     public static void main(String... args) throws Throwable {
         if (args.length == 0 || args[0].equals("--install")) {
@@ -33,15 +33,15 @@ public class Main {
                 System.out.println("File 'server.jar' does not exist");
                 System.out.println("Choose which server you'd like to download:");
                 System.out.println("  1) Vanilla");
-                System.out.println("  2) Spigot");
+                // System.out.println("  2) Spigot");
                 System.out.print("Choice: ");
 
                 URL url;
                 String line = new Scanner(System.in).nextLine().toLowerCase();
                 if (line.startsWith("1") || line.startsWith("v")) {
                     url = new URL(VANILLA_SERVER);
-                } else if (line.startsWith("2") || line.startsWith("s")) {
-                    url = new URL(SPIGOT_SERVER);
+//                } else if (line.startsWith("2") || line.startsWith("s")) {
+//                    url = new URL(SPIGOT_SERVER);
                 } else {
                     System.err.println("Not a valid choice");
                     return;
@@ -53,7 +53,7 @@ public class Main {
 
             addURLToClasspath(serverJar.toURI().toURL());
 
-            for (String url : libraries) {
+            for (String url : LIBRARIES) {
                 addURLToClasspath(getOrDownload(new File("libs"), new URL(url)).toURI().toURL());
             }
 
@@ -95,42 +95,79 @@ public class Main {
 
         try {
             File minecraftFolder;
-            String userHome = System.getProperty("user.home");
             String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
             if (osName.contains("win")) {
-                minecraftFolder = new File(userHome + "/AppData/Roaming/.minecraft");
+                minecraftFolder = new File(System.getenv("APPDATA") + "/.minecraft");
             } else if (osName.contains("mac")) {
-                minecraftFolder = new File(userHome + "/Library/Application Support/minecraft");
+                minecraftFolder = new File(System.getProperty("user.home") + "/Library/Application Support/minecraft");
             } else {
-                minecraftFolder = new File(userHome + "/.minecraft");
+                minecraftFolder = new File(System.getProperty("user.home") + "/.minecraft");
             }
 
-            File target = new File(minecraftFolder, "versions/1.13-rift-@VERSION@/1.13-rift-@VERSION@.json");
-            target.getParentFile().mkdirs();
-            Files.copy(Main.class.getResourceAsStream("/profile.json"), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            // Copy the version json
+            File versionJson = new File(minecraftFolder, "versions/1.13-rift-@VERSION@/1.13-rift-@VERSION@.json");
+            versionJson.getParentFile().mkdirs();
+            Files.copy(Main.class.getResourceAsStream("/profile.json"), versionJson.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
+            // Make mods directory
+            try {
+                File modsFolder = new File(minecraftFolder, "mods");
+                modsFolder.mkdirs();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            // Add rift as a profile
+            try {
+                File profilesJson = new File(minecraftFolder, "launcher_profiles.json");
+                if (profilesJson.exists()) { // TODO: use gson instead
+                    String contents = new String(Files.readAllBytes(profilesJson.toPath()));
+                    if (contents.contains("\"rift\"")) {
+                        contents = contents.replaceAll("\n *\"rift\": \\{[^{]*},", "");
+                    }
+
+                    contents = contents.replace("\n  \"profiles\": {", "\n  \"profiles\": {\n" +
+                                                                       "    \"rift\": {\n" +
+                                                                       "      \"name\": \"Rift\",\n" +
+                                                                       "      \"type\": \"custom\",\n" +
+                                                                       "      \"created\": \"2018-08-13T00:00:00.000Z\",\n" +
+                                                                       "      \"lastUsed\": \"2100-01-01T00:00:00.000Z\",\n" +
+                                                                       "      \"lastVersionId\": \"1.13-rift-@VERSION@\"\n" +
+                                                                       "    },");
+
+                    Files.write(profilesJson.toPath(), contents.getBytes());
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            // Copy rift jar to libraries
             try {
                 String source = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
                 if (source.startsWith("/")) {
                     source = source.substring(1);
                 }
-                target = new File(minecraftFolder, "libraries/org/dimdev/rift/@VERSION@/rift-@VERSION@.jar");
-                target.getParentFile().mkdirs();
-                Files.copy(Paths.get(source), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                File riftJar = new File(minecraftFolder, "libraries/org/dimdev/rift/@VERSION@/rift-@VERSION@.jar");
+                riftJar.getParentFile().mkdirs();
+                Files.copy(Paths.get(source), riftJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
             } catch (Throwable t) {
                 t.printStackTrace();
             }
 
             JOptionPane.showMessageDialog(null,
-                    "Rift @VERSION@ for Minecraft 1.13 has been successfully installed!\n\n" +
-                    "It is available in the \"Version\" dropdown menu in the \"Launch Options\"\n" +
-                    "section of the launcher.", "Rift Installer", JOptionPane.INFORMATION_MESSAGE);
+                    "Rift @VERSION@ for Minecraft 1.13 has been successfully installed!\n" +
+                    "\n" +
+                    "It is available in the dropdown menu of the vanilla Minecraft launcher.\n" +
+                    "You'll need to restart the Minecraft Launcher if you had it open when\n" +
+                    "you ran this installer.",
+                    "Rift Installer", JOptionPane.INFORMATION_MESSAGE);
         } catch (Throwable t) {
             StringWriter w = new StringWriter();
             t.printStackTrace(new PrintWriter(w));
             JOptionPane.showMessageDialog(null,
                     "An error occured while installing Rift, please report this to the issue\n" +
-                    "tracker (https://github.com/DimensionalDevelopment/Rift/issues):\n\n" +
+                    "tracker (https://github.com/DimensionalDevelopment/Rift/issues):\n" +
+                    "\n" +
                     w.toString().replace("\t", "    "), "Rift Installer", JOptionPane.ERROR_MESSAGE);
         }
     }
