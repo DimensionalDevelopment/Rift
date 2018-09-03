bd05daad0a6ca7280e23634b9c580d03fecf2c34
gbui <gbui@users.noreply.github.com>
2018-09-02 22:08:54 -0700

    Add IArmorMaterial and IItemTier implementations

---
+++ src/main/java/org/dimdev/rift/util/ArmorMaterialImpl.java
@@ -0,0 +1,65 @@
+package org.dimdev.rift.util;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyLoadBase;
+import net.minecraft.util.SoundEvent;
+
+import java.util.function.Supplier;
+
+public class ArmorMaterialImpl implements IArmorMaterial {
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final LazyLoadBase<Ingredient> ingredient;
+
+    public ArmorMaterialImpl(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, Supplier<Ingredient> ingredientSupplier) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.ingredient = new LazyLoadBase<>(ingredientSupplier);
+    }
+
+    @Override
+    public int func_200896_a(EntityEquipmentSlot slot) {
+        return ArmorMaterial.MAX_DAMAGE_ARRAY[slot.getIndex()] * maxDamageFactor;
+    }
+
+    @Override
+    public int func_200902_b(EntityEquipmentSlot p_200902_1_) {
+        return damageReductionAmountArray[p_200902_1_.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return soundEvent;
+    }
+
+    @Override
+    public Ingredient func_200898_c() {
+        return ingredient.getValue();
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public float getToughness() {
+        return toughness;
+    }
+}
---
+++ src/main/java/org/dimdev/rift/util/ItemTierImpl.java
@@ -0,0 +1,55 @@
+package org.dimdev.rift.util;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyLoadBase;
+
+import java.util.function.Supplier;
+
+public class ItemTierImpl implements IItemTier {
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyLoadBase<Ingredient> ingredient;
+
+    public ItemTierImpl(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> ingredientSupplier) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.ingredient = new LazyLoadBase<>(ingredientSupplier);
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient func_200924_f() {
+        return ingredient.getValue();
+    }
+}
