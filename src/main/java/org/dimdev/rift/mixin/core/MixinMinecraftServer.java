d14e60f41ae3b779c73d227cae3548030643be8e
Janrupf <werbung.janrupf@t-online.de>
2018-09-24 18:39:54 +0200

    Fix datapack loading issue, fixes #28

--- src/main/java/org/dimdev/rift/mixin/core/MixinMinecraftServer.java
+++ src/main/java/org/dimdev/rift/mixin/core/MixinMinecraftServer.java
@@ -1,13 +1,38 @@
 package org.dimdev.rift.mixin.core;
 
+import net.minecraft.command.CommandSource;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.WorldServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftServer.class)
 public class MixinMinecraftServer {
+    @Shadow
+    public WorldServer[] worlds;
+
     @Overwrite
     public String getServerModName() {
         return "rift";
+    }
+
+    @Inject(
+            method = "getCommandSource",
+            at = @At(value = "NEW"),
+            cancellable = true
+    )
+    private void beforeNew(CallbackInfoReturnable<CommandSource> ci) {
+        if(worlds == null) { // World has not been loaded yet, this might happen when the datapack is loaded for the first time
+            ci.setReturnValue(new CommandSource((MinecraftServer) (Object) this, Vec3d.ZERO, Vec2f.ZERO, null, 4, "Server",
+                    new TextComponentString("Server"), (MinecraftServer) (Object) this, null));
+            ci.cancel();
+        }
     }
 }
