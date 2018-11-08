30ec670f6d1915915bdc535bf4f19ad5779e6e9b
Unknown <lo9tt@hotmail.com>
2018-11-08 15:17:00 +0000

    Fix block and fluid listeners not firing

--- src/main/java/org/dimdev/rift/mixin/hook/MixinFluid.java
+++ src/main/java/org/dimdev/rift/mixin/hook/MixinFluid.java
@@ -3,6 +3,7 @@ package org.dimdev.rift.mixin.hook;
 import net.minecraft.fluid.Fluid;
 import org.dimdev.rift.listener.FluidAdder;
 import org.dimdev.riftloader.RiftLoader;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Fluid.class)
 public abstract class MixinFluid {
-    @Inject(method = "registerAll", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/registry/RegistryNamespacedDefaultedByKey;validateKey()V"))
+	@Inject(method = "registerAll", at = @At(value = "FIELD", target = "Lnet/minecraft/util/registry/IRegistry;FLUID:Lnet/minecraft/util/registry/IRegistry;", opcode = Opcodes.GETSTATIC, ordinal = 1))
     private static void onRegisterFluids(CallbackInfo ci) {
         for (FluidAdder fluidAdder : RiftLoader.instance.getListeners(FluidAdder.class)) {
             fluidAdder.registerFluids();
