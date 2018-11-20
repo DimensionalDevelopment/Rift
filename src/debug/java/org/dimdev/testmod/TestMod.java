145bec2712f1886af6f1b76591dbbc08b8afe00f
Unknown <lo9tt@hotmail.com>
2018-11-20 20:00:23 +0000

    Add the AmbientMusic test back again
    It's less annoying now, but still worth testing to make sure enum extending is fine

--- src/debug/java/org/dimdev/testmod/TestMod.java
+++ src/debug/java/org/dimdev/testmod/TestMod.java
@@ -7,7 +7,7 @@ import net.minecraft.block.BlockFlowingFluid;
 import net.minecraft.block.BlockStainedGlass;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.MusicTicker;
+import net.minecraft.client.audio.MusicTicker.MusicType;
 import net.minecraft.command.CommandSource;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.fluid.Fluid;
@@ -32,7 +32,7 @@ import java.util.Collections;
 
 import static net.minecraft.init.SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP;
 
-public class TestMod implements BlockAdder, ItemAdder, FluidAdder, TextureAdder, PacketAdder, CommandAdder, ClientTickable, /*AmbientMusicTypeProvider,*/ DimensionTypeAdder, MessageAdder {
+public class TestMod implements BlockAdder, ItemAdder, FluidAdder, TextureAdder, PacketAdder, CommandAdder, ClientTickable, AmbientMusicTypeProvider, DimensionTypeAdder, MessageAdder {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final Block WHITE_BLOCK = new Block(Block.Properties.create(Material.ROCK));
     public static final Block TRANSLUCENT_WHITE_BLOCK = new BlockStainedGlass(EnumDyeColor.WHITE, Block.Properties.create(Material.GLASS));
@@ -40,8 +40,8 @@ public class TestMod implements BlockAdder, ItemAdder, FluidAdder, TextureAdder,
     public static final FlowingFluid FLOWING_WHITE_FLUID = new WhiteFluid.Flowing();
     public static final BlockFlowingFluid BLOCK_WHITE_FLUID = new BlockFlowingFluid(WHITE_FLUID, Block.Properties.create(Material.WATER).doesNotBlockMovement().hardnessAndResistance(100F, 100F).variableOpacity());
     public static final Item PACKET_TESTER = new ItemPacketTester(new Item.Properties());
-    public static final MusicTicker.MusicType TEST_MUSIC = AmbientMusicTypeProvider.newMusicType("test", ENTITY_EXPERIENCE_ORB_PICKUP, 0, 0);
-    private int clientTickCount = 0;
+    public static final MusicType TEST_MUSIC = AmbientMusicTypeProvider.newMusicType("test", ENTITY_EXPERIENCE_ORB_PICKUP, 0, 0);
+    private int clientTickCount = 0, inWorldTicks = 0;
 
     @Override
     public void registerBlocks() {
@@ -93,11 +93,17 @@ public class TestMod implements BlockAdder, ItemAdder, FluidAdder, TextureAdder,
         if (clientTickCount++ == 100) {
             LOGGER.info("100 ticks have passed");
         }
+        if (client.world != null) {
+        	inWorldTicks++;
+        } else if (inWorldTicks > 0) {
+        	inWorldTicks = 0;
+        }
     }
 
-//    @Override
-    public MusicTicker.MusicType getAmbientMusicType(Minecraft client) {
-        return TEST_MUSIC;
+    @Override
+    public MusicType getAmbientMusicType(Minecraft client) {
+    	//Make some experience noises for 100 ticks but then stop because they are very annoying
+        return inWorldTicks < 100 ? TEST_MUSIC : null;
     }
 
     @Override
