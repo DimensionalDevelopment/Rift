c2d7e6cdc8253f6460f85b142430f7616ec13fb4
Unknown <shekwancheung0528@gmail.com>
2018-12-21 15:02:06 +0800

    Really basic list

--- src/main/java/org/dimdev/utils/ModList.java
+++ src/main/java/org/dimdev/utils/ModList.java
@@ -1,4 +1,19 @@
 package org.dimdev.utils;
 
+import net.minecraft.client.Minecraft;
+
 public class ModList {
+	
+	static GuiMods guiModsList;
+	
+	public static void displayModList() {
+		if (guiModsList == null)
+			guiModsList = new GuiMods();
+		else
+			guiModsList.getGuiModListContent().setCurrentIndex(-1);
+		guiModsList.previousGui = Minecraft.getInstance().currentScreen;
+		guiModsList.reloadSearch();
+		Minecraft.getInstance().displayGuiScreen(guiModsList);
+	}
+	
 }
